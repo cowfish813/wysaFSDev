1a7fe9990e50d9b14dd7c685f68154e751dd71a2
Nick_Cheung <nkkcheung@gmail.com>
2022-08-16 16:53:11 -0700

    st

--- js.js
+++ js.js
@@ -1,2 +1,2 @@
-FS dev mini proj
-https://docs.google.com/document/d/1rq8qU7S7OMGfWLR_w5fTapiaUsBoXTEMBUfeXPTKy8k/edit
+// FS dev mini proj
+// https://docs.google.com/document/d/1rq8qU7S7OMGfWLR_w5fTapiaUsBoXTEMBUfeXPTKy8k/edit
